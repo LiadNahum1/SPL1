67a38f0dc18a845ab78ce32844b12dcd7b61a6e1
liad <liadna@post.bgu.ac.il>
2018-11-05 15:55:51 +0200

    initial commit

---
+++ include/Restaurant.h
@@ -0,0 +1,32 @@
+#ifndef RESTAURANT_H_
+#define RESTAURANT_H_
+
+#include <vector>
+#include <string>
+#include "Dish.h"
+#include "Table.h"
+#include "Action.h"
+
+
+class Restaurant{
+public:
+    Restaurant();
+    Restaurant(const std::string &configFilePath);
+    void start();
+    int getNumOfTables() const;
+    Table* getTable(int ind);
+    const std::vector<BaseAction*>& getActionsLog() const; // Return a reference to the history of actions
+    std::vector<Dish>& getMenu();
+
+private:
+    bool open;
+    bool tableDe;
+    bool tableNum;
+    bool menuDe;
+    std::vector<Table*> tables;
+    std::vector<Dish> menu;
+    std::vector<BaseAction*> actionsLog;
+    void openTable(std::string a , int tableNum);
+};
+
+#endif
---
+++ src/Customer.cpp
@@ -0,0 +1,11 @@
+//
+// Created by liad on 11/2/18.
+//
+
+#include "Customer.h"
+#include <iostream>
+
+int Restaurant :: getNumOfTables() const{
+    tables.size();
+
+}
