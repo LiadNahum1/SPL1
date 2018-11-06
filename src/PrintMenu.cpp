c67fc84e4aeefcafe96bd1426fa649c627e140ed
liad <liadna@post.bgu.ac.il>
2018-11-06 14:57:33 +0200

    ..print menu

---
+++ src/PrintMenu.cpp
@@ -0,0 +1,19 @@
+//
+// Created by liad on 11/5/18.
+//
+#include <string>
+#include <iostream>
+#include "../include/Action.h"
+
+
+PrintMenu :: PrintMenu(){}
+void PrintMenu :: act(Restaurant &restaurant) {
+    vector<Dish> &menu = restaurant.getMenu();
+    for (int i = 0; i < menu.size() ; ++i) {
+        cout << menu.at(i).getName() + " " ;
+        cout << menu.at(i).getType() + " " ;
+        cout << menu.at(i).getPrice() + "NIS" << endl;
+
+    }
+}
+std::string PrintMenu :: toString() const{}
